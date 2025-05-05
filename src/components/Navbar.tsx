6f614283ca89fa12590975babd0c7383478a3d9f
emyrinj <emirilhaann@gmail.com>
2025-05-05 19:20:23 +0300

    Initial Divina Jewellery deploy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react"; // iconlar için (npm install lucide-react)
 
